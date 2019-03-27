d866df9709202db746da7e4daa203679ab5f7a93
Haker <Haker@HOME>
2019-03-27 23:24:37 +0300

    Тест абстрактного класса.

---
+++ Test/src/GameWorld/Abstract_Class.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package GameWorld;
+
+/**
+ * @author Haker
+ *
+ */
+
+abstract class A {
+	  void method() {
+	  }
+	}
+
+	interface B {
+	  void method();
+	}
+
+	class C extends A implements B {
+
+		public void method() {
+			
+		}
+		
+		
+		
+	}
+public class Abstract_Class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
